1397393e06e5a677c097b7c09e1da813d4fa79bd
Saulux2005 <97383079+Saulux2005@users.noreply.github.com>
2022-06-13 22:09:00 +0300

    Initial commit

---
+++ src/main/java/lt/bit/products/store/model/Product.java
@@ -0,0 +1,56 @@
+package lt.bit.products.store.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "store_ptoducts")
+public class Product {
+
+  @Id
+  private Integer id;
+  private String name;
+  private String description;
+  private LocalDate created;
+
+  public Product(Integer id, String name, String description, LocalDate created) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.created = created;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public LocalDate getCreated() {
+    return created;
+  }
+
+  public void setCreated(LocalDate created) {
+    this.created = created;
+  }
+}
